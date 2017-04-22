5c1e46b9c2fc00446fb536d5f09eb04e07433fe3
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2017-04-22 19:55:25 -0300

    Resolución parcial con argumento "--sort:asc" y "--sort:des"

---
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/FactoresPrimos.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class FactoresPrimos {
+	
+	static List<Integer> divisores;
+	
+	public static void main(String[] args) {
+		
+		divisores = new ArrayList<Integer>();
+		
+		int numero=Integer.parseInt(args[0]);
+		
+		
+		
+		if(args[2].toLowerCase().equals("--sort=des")){
+			if(args[1].toLowerCase().equals("--format=pretty")){
+				factoresPrimosPretty(numero);
+				prettyDescendente();
+			}else {
+				factoresPrimosQuiet(numero);
+				quietDescendente();
+			}
+		}else {
+			
+			if(args[1].toLowerCase().equals("--format=pretty")){
+				factoresPrimosPretty(numero);
+				prettyAscendente();
+			}else{
+				factoresPrimosQuiet(numero);
+				quietAscendente();
+				
+				
+			}
+		}
+		
+	}
+	
+	
+	static void factoresPrimosPretty(int numero){
+		int divisor=2;
+		System.out.print("Factores primos "+numero+" : ");
+		if(numero > 1){ 
+			for(int i = 0; i <= numero; i++) { 
+				if (numero % divisor == 0) { 
+					divisores.add(divisor); 
+					numero = numero/divisor; 
+				}else{ 
+					divisor++; 
+				} 
+			}
+		} 
+		
+			
+	}
+
+
+	private static void prettyAscendente() {
+		for (int i = 0; i < divisores.size(); i++) {
+			System.out.print(divisores.get(i)+" "); 
+		}
+		System.out.println();
+	}
+	
+
+	private static void prettyDescendente() {
+		Collections.reverse(divisores);
+		for (int i = 0; i < divisores.size(); i++) {
+			System.out.print(divisores.get(i)+" "); 
+		}
+		System.out.println();
+	}
+	
+	static void factoresPrimosQuiet(int numero){
+		int divisor=2;
+		if(numero > 1){ 
+			for (int i = 0; i <= numero; i++) { 
+				if (numero % divisor == 0) { 
+					divisores.add(divisor);
+					numero = numero/divisor; 
+				}else{ 
+					divisor++; 
+				} 
+			}
+		}
+	
+	}
+
+	private static void quietDescendente() {
+		Collections.reverse(divisores);
+		for (int i = 0; i < divisores.size(); i++) {
+			System.out.println(divisores.get(i)); 
+		}
+	}
+	
+	private static void quietAscendente() {
+	
+		for (int i = 0; i < divisores.size(); i++) {
+			System.out.println(divisores.get(i)); 
+		}
+	}
+}
---
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/FactoresPrimosTest.java
@@ -0,0 +1,180 @@
+import org.junit.Test;
+
+
+public class FactoresPrimosTest {
+
+
+	
+	@Test
+	public void factoresDe25ConFormatoQuietAscendente(){
+		String[]argumentos={"25", "--format=quiet", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe25ConFormatoQuietDescendente(){
+		String[]argumentos={"25", "--format=quiet", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe25ConFormatoPrettyAscendente(){
+		String[]argumentos={"25", "--format=pretty", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe25ConFormatoPrettyDescendente(){
+		String[]argumentos={"25", "--format=pretty", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+
+	
+	@Test
+	public void factoresDe14ConFormatoQuietAscendente(){
+		String[]argumentos={"14", "--format=quiet", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe14ConFormatoQuietDescendente(){
+		String[]argumentos={"14", "--format=quiet", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	@Test
+	public void factoresDe14ConFormatoPrettyAscendente(){
+		String[]argumentos={"14", "--format=pretty", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe14ConFormatoPrettyDescendente(){
+		String[]argumentos={"14", "--format=pretty", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	
+	
+	
+	@Test
+	public void factoresDe9ConFormatoQuietAscendente(){
+		String[]argumentos={"9", "--format=quiet", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe9ConFormatoQuietDescendente(){
+		String[]argumentos={"9", "--format=quiet", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe95ConFormatoPrettyAscendente(){
+		String[]argumentos={"9", "--format=pretty", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe95ConFormatoPrettyDescendente(){
+		String[]argumentos={"9", "--format=pretty", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe85ConFormatoQuietAscendente(){
+		String[]argumentos={"85", "--format=quiet", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe85ConFormatoQuietDescendente(){
+		String[]argumentos={"85", "--format=quiet", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe85ConFormatoPrettyAscendente(){
+		String[]argumentos={"85", "--format=pretty",  "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe85ConFormatoPrettyDescendente(){
+		String[]argumentos={"85", "--format=pretty", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	
+	@Test
+	public void factoresDe62ConFormatoQuietAscendete(){
+		String[]argumentos={"62", "--format=quiet", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	@Test
+	public void factoresDe62ConFormatoQuietDescendete(){
+		String[]argumentos={"62", "--format=quiet", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	@Test
+	public void factoresDe62ConFormatoPrettyAscendente(){
+		String[]argumentos={"62", "--format=pretty", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe62ConFormatoPrettyDescendente(){
+		String[]argumentos={"62", "--format=pretty", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	
+	@Test
+	public void factoresDe230ConFormatoQuietAscendente(){
+		String[]argumentos={"230", "--format=quiet", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	@Test
+	public void factoresDe230ConFormatoQuietDescendente(){
+		String[]argumentos={"230", "--format=quiet", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	@Test
+	public void factoresDe230ConFormatoPrettyAscendente(){
+		String[]argumentos={"230", "--format=pretty", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	@Test
+	public void factoresDe230ConFormatoPrettyDescendente(){
+		String[]argumentos={"230", "--format=pretty", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	
+	
+	@Test
+	public void factoresDe7787ConFormatoQuietAscendente(){
+		String[]argumentos={"7787", "--format=quiet", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	@Test
+	public void factoresDe7787ConFormatoQuietDescendente(){
+		String[]argumentos={"7787", "--format=quiet", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	@Test
+	public void factoresDe7787ConFormatoPrettyAscendente(){
+		String[]argumentos={"7787", "--format=pretty", "--sort=asc"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	@Test
+	public void factoresDe7787ConFormatoPrettyDescendente(){
+		String[]argumentos={"7787", "--format=pretty", "--sort=des"};
+		FactoresPrimos.main(argumentos);
+	}
+	
+	
+
+	
+}
